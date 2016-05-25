c5cb2d72bc71dc1415cc1ace2cf7fc5f5d89ace2
Luis Perez <luis.luimarin@gmail.com>
2016-05-25 15:18:39 -0600

    refactoring with pep8 info_hds.py

--- info_hds.py
+++ info_hds.py
@@ -1,7 +1,4 @@
-__author__ = 'binary'
 from funciones import print_info, connect
 
-api = connect('https://rhevm-r.itmlabs.local','admin@internal','S0p0rt3.')
-
-
-print_info(api,name='vm01-rhel6')
+api = connect('https://rhevm-r.itmlabs.local', 'admin@internal', 'S0p0rt3.')
+print_info(api, name='vm01-rhel6')
