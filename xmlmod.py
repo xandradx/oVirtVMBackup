9799918b94bfdede0d350e8298f2c86ef9aae2af
Luis Perez <luis.luimarin@gmail.com>
2016-05-25 15:49:04 -0600

    refactoring with pep8 xmlmod.py

--- xmlmod.py
+++ xmlmod.py
@@ -1,16 +1,10 @@
-__author__ = 'binary'
 from xml.dom import minidom
 
-xml_doc = minidom.parse('/home/binary/87a6f03e-e368-437b-8348-abed48b3a69e.ovf')
+file_name = "/home/binary/87a6f03e-e368-437b-8348-abed48b3a69e.ovf"
+xml_doc = minidom.parse(file_name)
 Content = xml_doc.getElementsByTagName('Content')
 for node in Content:
     section = node.getElementsByTagName('Section')
     for sect in section:
         item = sect.getElementsByTagName('Item')
-        print item.childNodes[0].nodeValue
-        #for mac in item:
-        #    macs = mac.getElementsByTagName('rasd:MACAddress')
-        #    print macs
-        #    macs = mac[0].nodeValue
-        #    print macs
-
+        print(item.childNodes[0].nodeValue)
