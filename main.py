366618b2ef4a46abea86a6d0232e014b77054143
Luis Perez <luis.luimarin@gmail.com>
2016-05-30 17:57:38 -0600

    realizando funcion main para el programa principal. se hara uso de argparse para manejor de opciones de Linea de Comandos

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 from ovirtvmbackup.ovirtbackup import OvirtBackup
+import argparse
 
-oVirt = OvirtBackup('https://rhevm.i-t-m.local', 'lperez@itmlabs.local', 'lab2016.')
-print("Conectando...")
-oVirt.connect()
-#oVirt.create_snap('Inicial', 'centos')
-#print("Eliminando snapshot...")
-#oVirt.delete_snap('InstallOK', 'Guacamole')
-oVirt.get_ovf('Guacamole', 'lab2016')
+def main(manager, user, password):
+    if manager is None:
+        exit(0)
+        url = "https://" + manager
+    oVirt = OvirtBackup(url, 'lperez@itmlabs.local', 'lab2016.')
+#    print("Conectando...")
+#    oVirt.connect()
+    # oVirt.create_snap('Inicial', 'centos')
+    # print("Eliminando snapshot...")
+    # oVirt.delete_snap('InstallOK', 'Guacamole')
+#    oVirt.get_ovf('Guacamole', 'lab2016')
+    print(url)
+    print(user)
+    print(password)
+
+if __name__ == '__main__':
+    main()
