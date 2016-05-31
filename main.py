ba97e1417dfe0850920b837a85825f247ed64127
Luis Perez <luis.luimarin@gmail.com>
2016-05-31 17:42:26 -0600

    crecion de las funciones get_args --> obtiene los argumentos desde linea de comandos export --> realiza el proceso de exportar la maquina virtual import --> realiza el proceso de import la maquina virtual

--- main.py
+++ main.py
@@ -1,20 +1,79 @@
 from ovirtvmbackup.ovirtbackup import OvirtBackup
+from colorama import init, Fore
 import argparse
 
-def main(manager, user, password):
-    if manager is None:
-        exit(0)
-        url = "https://" + manager
-    oVirt = OvirtBackup(url, 'lperez@itmlabs.local', 'lab2016.')
-#    print("Conectando...")
-#    oVirt.connect()
-    # oVirt.create_snap('Inicial', 'centos')
-    # print("Eliminando snapshot...")
-    # oVirt.delete_snap('InstallOK', 'Guacamole')
-#    oVirt.get_ovf('Guacamole', 'lab2016')
-    print(url)
-    print(user)
-    print(password)
+init(autoreset=True)
+
+def get_args():
+    '''Function parses and return arguments passed in'''
+
+    parser = argparse.ArgumentParser(
+        description='Automatic export/import virtual machine RHEV',
+        prog='ovirtbackup')
+
+    parser.add_argument('-v', '--version',
+                        action='version', version='%(prog)s 0.2')
+    parser.add_argument('--import', action="store_true", dest="imp",
+                        help="import virtual machine")
+    parser.add_argument('--export', help="export virtual machine",
+                        action="store_true")
+    parser.add_argument('vmname')
+    requiredNamed = parser.add_argument_group("required named arguments")
+    requiredNamed.add_argument('--manager', help='FQDN manager oVirt/RHEV',
+                               required=True)
+    requiredNamed.add_argument('--username', help='username with admin privileges',
+                               default='admin@internal')
+    requiredNamed.add_argument('--password', help='password for the user',
+                               required=True)
+
+    args = parser.parse_args()
+
+    import_vm = args.imp
+    export = args.export
+    vm_name = args.vmname
+    manager = args.manager
+    user = args.username
+    password = args.password
+    return import_vm, export, vm_name, manager, user, password
+
+def export(url, user, password, name, new_name, description):
+    print("Export virtual machine {}".format(name))
+
+    oVirt = OvirtBackup(url, user, password)
+    print("trying auth...")
+    if (oVirt.connect()):
+        print(Fore.GREEN + "auth OK")
+    if (oVirt.if_exists_vm(name)):
+        if (oVirt.if_exists_vm(new_name)):
+            print(Fore.RED + "Virtual Machine {} Backup already exists".format(new_name))
+        else:
+            print(Fore.YELLOW + "creating snapshot")
+            oVirt.create_snap(description, name)
+            print(Fore.GREEN + "create snapshot successful")
+            print(Fore.YELLOW + "creating new virtual machine {}".format(new_name))
+            oVirt.create_vm_to_export(name, new_name, description)
+    else:
+        print(Fore.RED + "Virtual Machine {} doesn't exists".format(name))
+
+def vm_import(name):
+    print("Import virtual machine {}".format(name))
+    pass
+
+def main():
+    is_import, is_export, name, manager, user, password = get_args()
+    new_name = name + "-snap"
+    description = "oVirtBackup"
+    url = "https://" + manager
+    if (is_import is not True) and (is_export is not True):
+        print(Fore.RED + "Export/Import option is necesary")
+        exit(1)
+    if (is_import) and (is_export):
+        print(Fore.RED + "Export or Import NOT must be combined")
+        exit(1)
+    elif (is_import):
+        vm_import(name)
+    elif (is_export):
+        export(url, user, password, name, new_name, description)
 
 if __name__ == '__main__':
     main()
