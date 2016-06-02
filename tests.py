f44aaba7bc02f70627c6babe02b27425732cd174
Luis Perez <luis.luimarin@gmail.com>
2016-06-02 14:48:56 -0600

    archivo test con uso de las funciones

--- tests.py
+++ tests.py
@@ -1,6 +1,23 @@
 from ovirtvmbackup.ovirtbackup import OvirtBackup
 
-ovirt = OvirtBackup('https://rhevm.i-t-m.local', 'lperez@itmlabs.local', 'lab2016.')
+DC_NAME =       'ITM'
+CLUSTER_NAME =  'CProd'
+HOST_NAME =     'vmnode01.i-t-m.local'
+STORAGE_NAME =  'DS01'
+EXPORT_NAME =   'Export'
+VM_NAME =       'fedora23-test'
+
+ovirt = OvirtBackup('https://rhevm.i-t-m.local/api', 'lperez@itmlabs.local', 'lab2016.')
 ovirt.connect()
 
-print(ovirt.get_vm_status('db01'))
+VM = ovirt.api.vms.get(VM_NAME)
+CLUSTER = ovirt.get_cluster(VM_NAME)
+DATACENTER = ovirt.get_dc(VM_NAME)
+
+STORAGE_DOMAINS = ovirt.get_storage_domains(VM_NAME)
+
+print("VM id: {} name: {}".format(VM.id, VM.name))
+print("CLUSTER id: {} name: {}".format(CLUSTER.id, CLUSTER.name))
+print("DATACENTER id: {} name: {}".format(DATACENTER.id, DATACENTER.name))
+for st in STORAGE_DOMAINS:
+    print("STORAGE id: {} name: {} type: {}".format(st.id, st.name, st.type_))
