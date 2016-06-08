57e96398ca8977f0dfcc4f21348de5c995a9f441
Luis Perez <luis.luimarin@gmail.com>
2016-06-08 16:22:31 -0600

    creando las funciones para creacion del arbol de directorios

--- mvfiles.py
+++ mvfiles.py
@@ -15,7 +15,7 @@ disk_id2 = "2f0c7f3b-7a65-439f-a55f-e258057c5b86"
 
 storage_id = "e412a6d5-8d62-4df5-a71c-352c50287a55/"
 vm_id = "696121ff-d098-4b3b-bd23-f357037608b1"
-vm_name = "Web01"
+virtual_machine = "Guacamole"
 path1 = export_path + storage_id
 
 
@@ -32,10 +32,6 @@ for np in paths:
 """
 
 dir_to_copy = list()
-
-#os.chdir(export_path + storage_id)
-os.chdir(export_path)
-print(os.getcwd())
 
 for root, directories, filenames in os.walk(os.getcwd()):
     for directory in directories:
@@ -54,28 +50,33 @@ for root, directories, filenames in os.walk(os.getcwd()):
             dir_to_copy.append(root)
 
 
-print(dir_to_copy)
+#print(dir_to_copy)
+
+def create_dirs(vm_name, export_path):
+    vms_path = 'master/vms/'
+    images_path = 'images/'
+    if not os.chdir(export_path):
+        if not os.mkdir(vm_name):
+            if not os.chdir(vm_name):
+                if not os.mkdir(images_path):
+                    os.makedirs(vms_path + vm_id)
+    else:
+        print("Error to create dirs")
+
+#create_dirs(virtual_machine, export_path)
+destination_dir = export_path + virtual_machine + '/master/vms/' + vm_id
+
+os.chdir(export_path + storage_id + 'master/vms/')
+print(os.listdir(os.getcwd()))
+#shutil.copytree(vm_id, destination_dir)
 
 
 
-def copy_vm():
+def create_tar():
     for dir_saved in dir_to_copy:
         try:
             import tarfile
-            with tarfile.open(export_path + vm_name + ".tar", "a") as tar:
+            with tarfile.open(export_path + virtual_machine + ".tar", "a") as tar:
                 tar.add(dir_saved)
         except OSError as e:
             print(e)
-        #copytree(dir_saved, export_path)
-#    try:
-        #os.mkdir(export_path + vm_name)
-#        shutil.copytree('/exportdomain/e412a6d5-8d62-4df5-a71c-352c50287a55/master/vms/696121ff-d098-4b3b-bd23-f357037608b1',
-#                export_path + vm_name + '/696121ff-d098-4b3b-bd23-f357037608b1')
-
-        # funciona!!!!
-        #shutil.copytree('/exportdomain/e412a6d5-8d62-4df5-a71c-352c50287a55/',
-        #            export_path + vm_name)
-#    except OSError as e:
-#        print(e)
-
-copy_vm()
