4890015ab1cc780cf971339fc6ab08283dfeb2db
Luis Perez <luis.luimarin@gmail.com>
2016-06-09 17:33:52 -0600

    Version funcional y lista para crear la estructura de los archivos correspondientes.

--- mvfiles.py
+++ mvfiles.py
@@ -1,59 +1,12 @@
+from __future__ import print_function
 import os
 from ovirtvmbackup import OvirtBackup
 import shutil
 
-manage = OvirtBackup('https://rhevm.i-t-m.local', 'lperez@itmlabs.local', 'lab2016.')
-manage.connect()
-
-disks = manage.api.vms.get('Web01').disks.list()
-
-pwd = os.path.dirname(os.path.abspath(__file__))
-
-export_path = "/exportdomain/"
-disk_id1 = "d16b86a8-0f7b-4777-a4da-22380b1fd386"
-disk_id2 = "2f0c7f3b-7a65-439f-a55f-e258057c5b86"
-
-gid_1 = 'cff0f290-44df-42f7-bb1d-6714d76f2644'
-gid_2 = 'd3566356-dfff-4d02-b62a-9dca30c7839f'
-
-storage_id = "e412a6d5-8d62-4df5-a71c-352c50287a55/"
-vm_id = "696121ff-d098-4b3b-bd23-f357037608b1"
-virtual_machine = "Guacamole"
-path1 = export_path + storage_id
-
-
-ids = [vm_id, disk_id2, disk_id1]
-
-"""
-for np in paths:
-    if not os.path.exists(np):
-        try:
-            os.makedirs(np)
-            print("Path {} created.".format(np))
-        except OSError as e:
-            print("{}".format(e))
-"""
-
-dir_to_copy = list()
-
-for root, directories, filenames in os.walk(os.getcwd()):
-    for directory in directories:
-        #print(os.path.join(root, directory))
-        if directory in ids:
-#            print("Directory {} found!".format(vm_id))
-#            print(os.path.join(root, directory))
-#            print("------------------------------")
-            dir_to_copy.append(os.path.join(root,directory))
-    for filename in filenames:
-        #print(os.path.join(root, filename))
-        if filename in ids:
-#            print("File {} found print directory !".format(vm_id))
-#            print(root)
-#            print("------------------------------")
-            dir_to_copy.append(root)
-
+path_export = '/exportdomain/'
 vms_path = '/master/vms/'
 images_path = '/images/'
+export_name = 'LabExport'
 
 def create_dirs(vm_name, export_path):
     try:
@@ -62,24 +15,36 @@ def create_dirs(vm_name, export_path):
     except OSError as e:
         print(e)
 
-create_dirs(virtual_machine, export_path)
-destination_dir = export_path + virtual_machine + vms_path
-destination_dir_images = export_path + virtual_machine + images_path
+def mv_data(new_name, export, source, destination):
+    st = manage.get_export_domain(new_name)
+    if export_name == st.name:
+        dest = path_export + new_name + destination
+        os.chdir(export + st.id + destination)
+        shutil.move(source, dest)
 
-print(destination_dir)
-print(destination_dir_images)
-os.chdir(export_path + storage_id + vms_path)
-print(os.listdir(os.getcwd()))
-shutil.move(vm_id,destination_dir)
-os.chdir(export_path + storage_id + images_path)
-shutil.move(gid_1,destination_dir_images)
-shutil.move(gid_2,destination_dir_images)
+manage = OvirtBackup('https://rhevm.i-t-m.local', 'lperez@itmlabs.local', 'lab2016.')
+manage.connect()
 
-def create_tar():
-    for dir_saved in dir_to_copy:
-        try:
-            import tarfile
-            with tarfile.open(export_path + virtual_machine + ".tar", "a") as tar:
-                tar.add(dir_saved)
-        except OSError as e:
-            print(e)
+disks = manage.api.vms.get('Web01-snap').disks.list()
+vm = manage.api.vms.get('Web01-snap')
+
+objects = { "Disks": list(), "Vms": list() }
+
+objects["Vms"].append(vm.id)
+
+for disk in disks:
+    print("Disk {} ID: {}".format(disk.name, disk.id))
+    objects["Disks"].append(disk.id)
+
+print("VM {} ID: {}".format(vm.name, vm.id))
+print(objects)
+
+vm_new = vm.name
+
+create_dirs(vm.name, path_export)
+
+for disk in objects["Disks"]:
+    mv_data(vm_new, path_export, disk, images_path)
+
+for vm in objects["Vms"]:
+    mv_data(vm_new, path_export, vm, vms_path)
