5c8597e2567655d024b78e152be6ba1ea56eb5e2
Luis Perez <luis.luimarin@gmail.com>
2016-06-10 18:10:51 -0600

    moviendo la creacion del tag rasd:StorageId dentro del for para que se creee en cada nodo del XML

--- add_storageid.py
+++ add_storageid.py
@@ -2,8 +2,8 @@ from __future__ import print_function
 from xml.dom import minidom
 
 # file_name = "/home/binary/a583ffd9-3a88-4347-b9b2-5bbce28414f7.ovf"
-file_name_old = "/home/binary/696121ff-d098-4b3b-bd23-f357037608b1.ovf"
-file_name_new = "/home/binary/b2f27e88-71a9-4934-8ad1-8b933a7e7ff4.ovf"
+file_name_old = "/opt/testxml/696121ff-d098-4b3b-bd23-f357037608b1.ovf"
+file_name_new = "/opt/testxml/b2f27e88-71a9-4934-8ad1-8b933a7e7ff4.ovf"
 
 
 xml_doc = minidom.parse(file_name_old)
@@ -16,22 +16,18 @@ disks_new = xml_doc_new.getElementsByTagName('Disk')
 st_id = '5ae04d0f-93c7-48a8-810c-4c124e40802c'
 disks = list()
 
-StorageId = xml_doc.createElement("rasd:StorageId")
-content = xml_doc.createTextNode(st_id)
-StorageId.appendChild(content)
-
+#StorageId = xml_doc.createElement("rasd:StorageId")
+#content = xml_doc.createTextNode(st_id)
+#StorageId.appendChild(content)
+#print(StorageId.toxml())
 
 for item in xml_doc.getElementsByTagName("Device"):
+    StorageId = xml_doc.createElement("rasd:StorageId")
+    content = xml_doc.createTextNode(st_id)
+    StorageId.appendChild(content)
     if item.firstChild.nodeValue == "disk":
         disks.append(item)
         parent = item.parentNode
-        print("Current")
-        print("****************")
-        print(item.toxml())
-        print(parent.toxml())
-        print("*/*/*/*/*/*/**/*/*")
-        print("New")
-        print("****************")
         parent.appendChild(StorageId)
-        print(parent.toxml())
-        print("*/*/*/*/*/*/**/*/*")
+
+print(xml_doc.toxml())
