8043138bfbbcf6d06b649cf7f544295b82c48ae0
Luis Perez <luis.luimarin@gmail.com>
2016-06-13 11:40:52 -0600

    eliminando comentarios innecesarios

--- add_storageid.py
+++ add_storageid.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
 from xml.dom import minidom
 
-# file_name = "/home/binary/a583ffd9-3a88-4347-b9b2-5bbce28414f7.ovf"
+
 file_name_old = "/opt/testxml/696121ff-d098-4b3b-bd23-f357037608b1.ovf"
 file_name_new = "/opt/testxml/b2f27e88-71a9-4934-8ad1-8b933a7e7ff4.ovf"
 
@@ -11,15 +11,10 @@ xml_doc_new = minidom.parse(file_name_new)
 
 disks_old = xml_doc.getElementsByTagName('Disk')
 disks_new = xml_doc_new.getElementsByTagName('Disk')
-# print(disks)
-# <rasd:StorageId>5ae04d0f-93c7-48a8-810c-4c124e40802c</rasd:StorageId>
+
 st_id = '5ae04d0f-93c7-48a8-810c-4c124e40802c'
 disks = list()
 
-#StorageId = xml_doc.createElement("rasd:StorageId")
-#content = xml_doc.createTextNode(st_id)
-#StorageId.appendChild(content)
-#print(StorageId.toxml())
 
 for item in xml_doc.getElementsByTagName("Device"):
     StorageId = xml_doc.createElement("rasd:StorageId")
