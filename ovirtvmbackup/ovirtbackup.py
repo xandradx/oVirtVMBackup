f8e507d6da74077f4a3ae6786f5ec1dbe2a2243a
Luis Perez <luis.luimarin@gmail.com>
2016-06-13 11:07:39 -0600

    Agregando las funciones necesarias para el manejo de los Export Domain

--- ovirtvmbackup/ovirtbackup.py
+++ ovirtvmbackup/ovirtbackup.py
@@ -99,22 +99,6 @@ class OvirtBackup():
             print("Error: {} Reason: {}".format(err.status, err.reason))
             exit(0)
 
-    def get_export_domain(self, vm):
-        """Return Export Domain
-            :param vm: Virtual Machine Name
-        """
-        # self.cluster = self.api.clusters.get(id=self.api.vms.get(vm).cluster.id)
-        # self.dc = self.api.datacenters.get(id=self.cluster.data_center.id)
-        self.cluster = self.get_cluster(vm)
-        self.dc = self.get_dc(vm)
-
-        self.export = None
-
-        for self.sd in self.dc.storagedomains.list():
-            if self.sd.get_type() == "export":
-                self.export = self.sd
-        return self.export
-
     def get_storage_domains(self,vm):
         self.datacenter = self.get_dc(vm)
         return self.datacenter.storagedomains.list()
@@ -159,6 +143,74 @@ class OvirtBackup():
         except Exception as e:
             print(e.message)
             exit(1)
+            
+# Funciones de manejo de Exports Domains
+
+    def get_export_domain(self, vm):
+        """Return Export Domain
+            :param vm: Virtual Machine Name
+        """
+        # self.cluster = self.api.clusters.get(id=self.api.vms.get(vm).cluster.id)
+        # self.dc = self.api.datacenters.get(id=self.cluster.data_center.id)
+        self.cluster = self.get_cluster(vm)
+        self.dc = self.get_dc(vm)
+
+        self.export = None
+
+        for self.sd in self.dc.storagedomains.list():
+            if self.sd.get_type() == "export":
+                self.export = self.sd
+        return self.export
+
+    def verify_valid_export(self, dc_id, export, current):
+        if current == export:
+            if self.api.datacenters.get(id=dc_id).storagedomains.get(
+                    export).get_status().get_state() == "active":
+                return 1
+            else:
+                return 2
+        elif current != export:
+            return 0
+
+    def detach_export(self, dc_id, export):
+        if self.api.datacenters.get(id=dc_id).storagedomains.get(export).delete():
+            print("Detach OK")
+
+    def do_export_maintenance(self, dc_id, export):
+        self.api.datacenters.get(id=dc_id).storagedomains.get(export).deactivate()
+        spinner = Spinner("waiting for maintenance Storage... ")
+        while self.api.datacenters.get(id=dc_id).storagedomains.get(
+                export).get_status().get_state() != "maintenance":
+            spinner.next()
+
+    def attach_export(self, dc_id, export_ok):
+        if self.api.datacenters.get(id=dc_id).storagedomains.add(self.api.storagedomains.get(export_ok)):
+            print('Export Domain was attached successfully')
+
+    def do_export_up(self, dc_id, export):
+        if self.api.datacenters.get(id=dc_id).storagedomains.get(export).activate():
+            print('Export Domain was activate successfully')
+
+    def prepare_export(self, dc_id, vm, name_export):
+        print("Distinto")
+        self.do_export_maintenance(dc_id, name_export)
+        self.detach_export(dc_id, name_export)
+
+    def active_export(self, vm, export_name):
+        export_attached = self.get_export_domain(vm)
+        dc = self.get_dc(vm)
+        if export_attached is not None:
+            status_export = self.verify_valid_export(dc.id, export_name, export_attached.name)
+            if status_export == 1:
+                print("Export {} is OK".format(export_name))
+            elif status_export == 0:
+                self.prepare_export(dc.id, vm, export_attached.name)
+                self.attach_export(dc.id, export_name)
+            elif status_export == 2:
+                self.do_export_up(dc.id, export_name)
+        elif export_attached is None:
+            self.attach_export(dc.id, export_name)
+
 
 if __name__ == '__main__':
     print("This file is intended to be used as a library of functions and it's not expected to be executed directly")
