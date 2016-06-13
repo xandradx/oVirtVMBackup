ad3b07d524e79ff114c8c4bbfe67e79d2d9bf383
Luis Perez <luis.luimarin@gmail.com>
2016-06-13 11:40:45 -0600

    eliminando comentarios innecesarios

--- xmlmod.py
+++ xmlmod.py
@@ -1,23 +1,14 @@
 from __future__ import print_function
 from xml.dom import minidom
 
-# file_name = "/home/binary/a583ffd9-3a88-4347-b9b2-5bbce28414f7.ovf"
+
 file_name_old = "/home/binary/696121ff-d098-4b3b-bd23-f357037608b1.ovf"
 file_name_new = "/home/binary/b2f27e88-71a9-4934-8ad1-8b933a7e7ff4.ovf"
 xml_doc = minidom.parse(file_name_old)
 xml_doc_new = minidom.parse(file_name_new)
 
-"""
-Content = xml_doc.getElementsByTagName('Content')
-for node in Content:
-    section = node.getElementsByTagName('Section')
-    for sect in section:
-        item = sect.getElementsByTagName('Item')
-        print(item)
-"""
 disks_old = xml_doc.getElementsByTagName('Disk')
 disks_new = xml_doc_new.getElementsByTagName('Disk')
-# print(disks)
 
 disks_old[0].attributes["ovf:fileRef"].value
 
