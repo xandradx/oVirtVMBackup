2b6c1dbdc8e3fadfb18677c0b3caa84a0cab9441
Luis Perez <luis.luimarin@gmail.com>
2016-06-13 18:09:18 -0600

    creando funciones para el manejo de agregar la linea de rasd:StorageId en el XML original

--- add_storageid.py
+++ add_storageid.py
@@ -1,28 +1,45 @@
 from __future__ import print_function
 from xml.dom import minidom
+from ovirtvmbackup import OvirtBackup
+
+ovbackup = OvirtBackup(
+    "https://rhevm.i-t-m.local",
+    'lperez@itmlabs.local', 'lab2016.'
+)
+ovbackup.connect()
 
 
-file_name_old = "/opt/testxml/696121ff-d098-4b3b-bd23-f357037608b1.ovf"
-file_name_new = "/opt/testxml/b2f27e88-71a9-4934-8ad1-8b933a7e7ff4.ovf"
+def get_storage_xml(xml_export):
+    xml_tag = xml_export.getElementsByTagName("rasd:StorageId")
+    storage_ids = list()
+    for storage_id in xml_tag:
+        storage_ids.append(storage_id.firstChild.nodeValue)
+    return storage_ids
 
 
-xml_doc = minidom.parse(file_name_old)
-xml_doc_new = minidom.parse(file_name_new)
+def add_line_storage_xml(xml_original, xml_export):
 
-disks_old = xml_doc.getElementsByTagName('Disk')
-disks_new = xml_doc_new.getElementsByTagName('Disk')
+    xml_doc = minidom.parse(xml_original)
+    xml_doc_new = minidom.parse(xml_export)
 
-st_id = '5ae04d0f-93c7-48a8-810c-4c124e40802c'
-disks = list()
+    count = 0
+
+    for item in xml_doc.getElementsByTagName("Device"):
+        if item.firstChild.nodeValue == "disk":
+            st_id = get_storage_xml(xml_doc_new)
+            StorageId = xml_doc.createElement("rasd:StorageId")
+            content = xml_doc.createTextNode(st_id[count])
+            StorageId.appendChild(content)
+            parent = item.parentNode
+            parent.appendChild(StorageId)
+            count += 1
+    with open('new-a583ffd9-3a88-4347-b9b2-5bbce28414f7.ovf', 'a') as newfile:
+        newfile.write(xml_doc.toxml())
+    print(xml_doc.toprettyxml())
 
 
-for item in xml_doc.getElementsByTagName("Device"):
-    StorageId = xml_doc.createElement("rasd:StorageId")
-    content = xml_doc.createTextNode(st_id)
-    StorageId.appendChild(content)
-    if item.firstChild.nodeValue == "disk":
-        disks.append(item)
-        parent = item.parentNode
-        parent.appendChild(StorageId)
+ovbackup.save_ovf("Web01", "oVirtBackup")
 
-print(xml_doc.toxml())
+original_ovf = '/tmp/a583ffd9-3a88-4347-b9b2-5bbce28414f7.ovf'
+export_ovf = '/tmp/b2f27e88-71a9-4934-8ad1-8b933a7e7ff4.ovf'
+add_line_storage_xml(original_ovf, export_ovf)
