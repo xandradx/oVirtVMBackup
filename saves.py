796090c6698a9963f0fc67201472e270d8d8ae2a
Luis Perez <luis.luimarin@gmail.com>
2016-06-15 15:25:56 -0600

    modulo que maneja el renombrado y movimiento de informacion dentro del backup path

---
+++ saves.py
@@ -0,0 +1,28 @@
+from __future__ import print_function
+from ovirtvmbackup import OvirtBackup
+import fnmatch
+import os
+
+path_export = "/exportdomain/"
+vms_path = "/master/vms/"
+images_path = "/images/"
+
+virtual_machine="Web01"
+description="oVirtBackup"
+
+ovbackup = OvirtBackup(
+    "https://rhevm.i-t-m.local",
+    'lperez@itmlabs.local', 'lab2016.'
+)
+ovbackup.connect()
+
+# procedure for save new ovf
+ovbackup.get_running_ovf(vm=virtual_machine, desc=description, path=path_export)
+export_xml = ovbackup.export_xml_path(path=path_export, vm=virtual_machine, find_path=vms_path)
+original_xml = ovbackup.export_xml_path(path=path_export, vm=virtual_machine)
+xml_obj = ovbackup.add_storage_id_xml(original_xml, export_xml)
+ovf_final = os.path.basename(original_xml)[8:]
+vms_path_save = path_export + virtual_machine + vms_path
+print(vms_path_save)
+ovbackup.save_new_ovf(path=vms_path_save, name=ovf_final, xml=xml_obj)
+ovbackup.change_owner(path=path_export + virtual_machine)
