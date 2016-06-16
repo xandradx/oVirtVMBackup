4490f0c149f5137f5c5db715c3f775ee62215453
Luis Perez <luis.luimarin@gmail.com>
2016-06-16 09:59:34 -0600

    integrando procedimiento completo

--- backup.py
+++ backup.py
@@ -1,3 +1,5 @@
+import os
+
 from ovirtvmbackup import OvirtBackup, rename_clone
 from colorama import init, Fore
 import argparse
@@ -59,13 +61,22 @@ def export(conn, vm_name, new_name, description, export_domain):
             conn.export_vm(new_name, export_dom)
             print(Fore.GREEN + "\nExport Virtual Machine {} successful".format(export_domain))
             print(Fore.YELLOW + "Moving export to another location")
-            #conn.create_dirs(vm_name=vm_name, export_path=path_export, images=images_path, vms=vms_path)
             conn.do_mv(vm=new_name, export_path=path_export, images=images_path, vms=vms_path)
-
+            # trabajado con ovf's
+            conn.get_running_ovf(vm=vm_name, desc=description, path=path_export)
+            export_xml = conn.export_xml_path(path=path_export, vm=vm_name, find_path=vms_path)
+            original_xml = conn.export_xml_path(path=path_export, vm=vm_name)
+            xml_obj = conn.add_storage_id_xml(original_xml, export_xml)
+            ovf_final = os.path.basename(original_xml)[8:]
+            vms_path_save = path_export + vm_name + vms_path
+            conn.save_new_ovf(path=vms_path_save, name=ovf_final, xml=xml_obj)
+            conn.delete_tmp_ovf(path=path_export + vm_name + "/running-" + ovf_final)
+            rename_clone(export_xml, vms_path_save + ovf_final, path_export + vm_name + images_path)
             # Eliminando snapshot y {vm}-snap
             conn.delete_snap(vm=vm_name, desc=description)
             conn.delete_tmp_vm(new_name=new_name)
-
+            #  cambiando permisos
+            conn.change_owner(path=path_export + vm_name)
             print(Fore.GREEN + "\nMove successful")
             print(Fore.GREEN + "process finished successful")
     else:
