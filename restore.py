2845e7260e50663ae4ef9f8ef423c868c3e60925
Luis Perez <luis.luimarin@gmail.com>
2016-07-05 12:32:36 -0600

    terminando proceso de movimiento de archivos hacia export domain, funcion ovf_get devuelve 2 parametros uno el path completo del archivo ovf y otro el directorio del mismo. Se dividio la funcion restore y se creo otra llamada restore_imgs para la restauracion de de las imagenes de la maquina virtual dada
    creada la variable export_id, pero momentaneamente se coloca el id a mano, mientras se obtiene del API de RHEV

--- restore.py
+++ restore.py
@@ -14,18 +14,19 @@ def args():
     p.add_argument('-P', '--path-export', required=True, help='path of export domain')
     p.add_argument('vm', help='name of virtual machine')
 
-
     options = p.parse_args()
     export_domain = options.export_domain
     virtual_machine = options.vm
     export_path = options.path_export
     return export_domain, export_path, virtual_machine
 
+
 def ovf_get(vm_path):
     for root, dirs, files in os.walk(vm_path):
         for file in files:
             if file.endswith(".ovf"):
-                return os.path.join(root, file)
+                return os.path.join(root, file), root
+
 
 def parse_xml(xml_path):
     xml_ovf = minidom.parse(xml_path)
@@ -37,24 +38,32 @@ def parse_xml(xml_path):
     return dgroups
 
 
-def restore(export_name, path, vm_name):
-    imgs = os.path.join(path, vm_name, "images")
-    vms = os.path.join(path, vm_name, "master", "vms")
-    export_vms = os.path.join(path, "master", "vms")
-    export_imgs = os.path.join(path, "images")
-    ovf = ovf_get(vms)
-    disksg = parse_xml(ovf)
+def restore_imgs(disksg, imgs, export_imgs):
     for disk in disksg:
-        print(os.path.join(imgs, disk))
-        print(export_vms)
         disk_src = os.path.join(imgs, disk)
-        shutil.move(disk_src, export_vms)
-    print(disksg)
+        print("moving {} to {}".format(disk_src, export_imgs))
+        shutil.move(disk_src, export_imgs)
+
+
+def restore(export_name, path, vm_name):
+    try:
+        export_id = "e412a6d5-8d62-4df5-a71c-352c50287a55"
+        imgs = os.path.join(path, vm_name, "images")
+        vms = os.path.join(path, vm_name, "master", "vms")
+        export_imgs = os.path.join(path, export_id, "images")
+        ovf, dir_vm = ovf_get(vms)
+        disksg = parse_xml(ovf)
+        restore_imgs(disksg=disksg, imgs=imgs, export_imgs=export_imgs)
+        export_vms = os.path.join(path, export_id, "master", "vms")
+        shutil.move(dir_vm, export_vms)
+    except OSError as e:
+        print(e)
 
 
 def main():
     export, path, virtual_machine = args()
     restore(export_name=export, path=path, vm_name=virtual_machine)
+
 
 if __name__ == '__main__':
     main()
