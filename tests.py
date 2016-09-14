73f8eed5b0b65c2cb12c179ee6d423c15791d7a0
Luis Perez <luis.luimarin@gmail.com>
2016-09-14 12:03:28 -0600

    new tests file

--- tests.py
+++ tests.py
@@ -5,9 +5,9 @@ CLUSTER_NAME =  'CProd'
 HOST_NAME =     'vmnode01.i-t-m.local'
 STORAGE_NAME =  'DS01'
 EXPORT_NAME =   'LabExport'
-VM_NAME =       'Web01'
+VM_NAME =       'policydwebui'
 
-ovirt = OvirtBackup('https://rhevm.i-t-m.local/api', 'lperez@itmlabs.local', 'lab2016.')
+ovirt = OvirtBackup('https://rhevm.i-t-m.local/api', 'lperez@itmlabs.local', 'lab2016.', 'Web01')
 ovirt.connect()
 
 VM = ovirt.api.vms.get(VM_NAME)
@@ -16,14 +16,15 @@ DATACENTER = ovirt.get_dc(VM_NAME)
 
 STORAGE_DOMAINS = ovirt.get_storage_domains(VM_NAME)
 
-print("VM id: {} name: {}".format(VM.id, VM.name))
-print("CLUSTER id: {} name: {}".format(CLUSTER.id, CLUSTER.name))
-print("DATACENTER id: {} name: {}".format(DATACENTER.id, DATACENTER.name))
-for st in STORAGE_DOMAINS:
-    print("STORAGE id: {} name: {} type: {}".format(st.id, st.name, st.type_))
-
-print(ovirt.api.datacenters.get(id=DATACENTER.id).storagedomains.get(
-                EXPORT_NAME).get_status().get_state())
-
+# print("VM id: {} name: {}".format(VM.id, VM.name))
+# print("CLUSTER id: {} name: {}".format(CLUSTER.id, CLUSTER.name))
+# print("DATACENTER id: {} name: {}".format(DATACENTER.id, DATACENTER.name))
+# for st in STORAGE_DOMAINS:
+#    print("STORAGE id: {} name: {} type: {}".format(st.id, st.name, st.type_))
 
 print(ovirt.vm_state('TSMProxy'))
+print("Maquina virtual: {}".format(ovirt.virtual))
+
+
+# ovirt.verify_environment(path='/tmp', vm='Web01', export='LabExport')
+
